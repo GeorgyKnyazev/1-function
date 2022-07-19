cd84bafc87b367d2aaaaa7af8c9cd6a401bfbd7c
Георгий Князев <saint_georgy@yahoo.com>
2022-07-19 21:25:46 +0300

    1

--- 1 function/Program.cs
+++ 1 function/Program.cs
@@ -6,12 +6,10 @@ namespace _1_function
     {
         static void Main(string[] args)
         {
-            string[] firstNameMassiv = new string[0];
-            string[] secondNameMassiv = new string[0];
-            string firstname;
-            string secondname;
-            int[] searchFirstNameResult = new int[0];
-            int[] searchSecondNameResult = new int[0];
+            string[] postMassiv = new string[0];
+            string[] surNameMassiv = new string[0];
+            string post = " ";
+            string surName = " ";
 
             bool continueProgram = true;
             int userInput;
@@ -19,7 +17,6 @@ namespace _1_function
 
             while(continueProgram == true)
             {
-                
                 Console.SetCursorPosition(0,0);
                 Console.WriteLine("1 - Добавить досье");
                 Console.WriteLine("2 - Вывести все досье");
@@ -28,137 +25,103 @@ namespace _1_function
                 Console.WriteLine("5 - Выход");
 
                 userInput = Convert.ToInt32(Console.ReadLine());
+                //Console.Clear();
                 
                 switch (userInput)
                 {
                     case 1:
-                        Console.Write("Введите имя: ");
-                        firstname = Console.ReadLine();
-                        Console.WriteLine();
-                        Console.Write("Введите фамилию: ");
-                        secondname = Console.ReadLine();
-
-                        firstNameMassiv = WriteInMassiv(firstname, firstNameMassiv);
-                        secondNameMassiv = WriteInMassiv(secondname, secondNameMassiv);
-                        Console.Clear();
+                        AddDossier(ref surNameMassiv, ref postMassiv, surName, post);
                         break;
-
                     case 2:
-                        Console.SetCursorPosition(0, 15);
-                        OutputMassiv(firstNameMassiv, secondNameMassiv);
+                        OutputMassiv(surNameMassiv, postMassiv);
                         break;
-
                     case 3:
-                        Console.Clear();
-                        Console.Write("Введите номер досье которое хотите удалить: ");
-                        userInput = Convert.ToInt32(Console.ReadLine());
-
-                        if(userInput <= firstNameMassiv.Length)
-                        {
-                            firstNameMassiv = DeliteNameInMassiv(firstNameMassiv, userInput);
-                            secondNameMassiv = DeliteNameInMassiv(secondNameMassiv, userInput);
-                        }
-                        else
-                        {
-                            Console.WriteLine("нет досье с таким номером: " + userInput);
-                        }
-
+                        DeliteDossier(ref surNameMassiv, ref postMassiv, userInput);
                         break;
-
                     case 4:
-                        Console.WriteLine("Введите имя или фамилию кого хотите найти: ");
-                        nameForSearch = Console.ReadLine();
-                        searchFirstNameResult = SearshResult(nameForSearch, firstNameMassiv);
-                        searchSecondNameResult = SearshResult(nameForSearch, secondNameMassiv);
-                        
-                        if(searchFirstNameResult.Length >= 0)
-                        {
-                            Console.Write("Данное имя встречаеться в следующих досье: ");
-                            for (int i = 0; i < searchFirstNameResult.Length; i++)
-                            {
-                                Console.Write(searchFirstNameResult[i] + " ");
-                            }
-                        }
-                        else if(searchSecondNameResult.Length >= 0)
-                        {
-                            Console.Write("Данная фамилия встречаеться в следующих досье: ");
-                            for (int i = 0; i < searchSecondNameResult.Length; i++)
-                            {
-                                Console.Write(searchSecondNameResult[i] + " ");
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("Ничего не найдено");
-                        }
-
+                        SearshResult(surName, surNameMassiv);
                         break;
-
                     case 5:
-                        //for (int i = 0; i < firstNameMassiv.Length; i++)
-                        //{
-                        //    Console.WriteLine((i+1)+ " " +  firstNameMassiv[i] + " " + secondNameMassiv[i]);
-                        //}
                         continueProgram = false;
                         break;
                 }
             }           
         }
-        static string[] WriteInMassiv(string name, string[] massiv)
+
+        static void AddDossier(ref string[] massiv1, ref string[] massiv2, string surName, string post)
+        {
+            Console.Write("Введите фамилию: ");
+            surName = Console.ReadLine();
+            Console.Write("Введите должность: ");
+            post = Console.ReadLine();
+
+            massiv1 = WriteInMassiv(massiv1, surName);
+            massiv2 = WriteInMassiv(massiv2, post);
+            Console.Clear();
+        }
+        
+        static string[]  WriteInMassiv(string[] massiv, string variable)
         {
             string[] tempMassiv = new string[massiv.Length + 1];
             for (int i = 0; i < massiv.Length; i++)
             {
                 tempMassiv[i] = massiv[i];
             }
-            tempMassiv[tempMassiv.Length - 1] = name;
-            
+            tempMassiv[tempMassiv.Length - 1] = variable;
             return tempMassiv;
         }
-        static void OutputMassiv(string[] firstNameMassiv, string[] secondNameMassiv)
+        
+        static void OutputMassiv(string[] massiv1, string[] massiv2)
         {
-            for (int i = 0; i < firstNameMassiv.Length; i++)
+            Console.SetCursorPosition(0, 15);
+            for (int i = 0; i < massiv1.Length; i++)
             {
-                Console.WriteLine((i + 1) + " " + firstNameMassiv[i] + " " + secondNameMassiv[i]);
+                Console.WriteLine((i + 1) + " " + massiv1[i] + " " + massiv2[i]);
             }
         }
-        static string[] DeliteNameInMassiv(string[] massiv, int userInput)
+        
+        static void DeliteDossier(ref string[] massiv1, ref string[] massiv2, int userInput)
         {
-            int j = 0;
+            Console.Write("Введите номер досье для удаления: ");
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            massiv1 = DeliteInMassiv(massiv1, userInput);
+            massiv2 = DeliteInMassiv(massiv2, userInput);
+            Console.Clear();
+        }
+        
+        static string[] DeliteInMassiv(string[] massiv, int userInput)
+        {
+            int index = 0;
             string[] tempMassiv = new string[massiv.Length - 1];
             for (int i = 0; i < tempMassiv.Length; i++)
             {
-                if (j != (userInput - 1))
+                if (index != (userInput - 1))
                 {
-                    tempMassiv[i] = massiv[j];
-                    j++;
+                    tempMassiv[i] = massiv[index];
+                    index++;
                 }
                 else
                 {
-                    j++;
-                    tempMassiv[i] = massiv[j];
+                    index++;
+                    tempMassiv[i] = massiv[index];
                 }
             }
             return tempMassiv;
         }
-        static int[] SearshResult(string name, string[] massiv)
+        static void SearshResult(string name, string[] massiv)
         {
-            int[] firstTempMassiv = new int[0];
-            
+            Console.Write("Введите фамилию для поиска: ");
+            name = Console.ReadLine();
+            Console.Write("Дааная фамилия встречаеться в следующих досье: ");
+
             for (int i = 0; i < massiv.Length; i++)
             {
                 if (massiv[i].ToLower() == name.ToLower())
                 {
-                    int[] secondTempMassiv = new int[firstTempMassiv.Length + 1];
-                    for(int j = 0; j < secondTempMassiv.Length - 1 ; j++)
-                    {
-                        secondTempMassiv[j] = firstTempMassiv[j];
-                    }
-                    secondTempMassiv[secondTempMassiv.Length - 1] = (i + 1);
-                    firstTempMassiv = secondTempMassiv;
+                    Console.Write((i+1) + " ");
                 }
             }
-            return firstTempMassiv;
         }
     }
 }
